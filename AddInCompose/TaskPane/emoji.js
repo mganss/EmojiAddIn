efdc47f2117d1b0e74396f935ed2f7bca84faf2e
Michael Ganss <michael.ganss@updatestar.com>
2017-04-21 13:09:52 +0200

    Update to EmojiOne 3.0

--- AddInCompose/TaskPane/emoji.js
+++ AddInCompose/TaskPane/emoji.js
@@ -4,11 +4,14 @@ Office.initialize = function () {
             localStorage: window.localStorage,
             createEmojiImage: function (e) {
                 var val = e.Value;
+                var unicode = e.Key;
                 var span = document.createElement("span");
-                var classes = ["emojione emojione-" + val.unicode, val.category];
+                var sheet = typeof val.tone !== 'undefined' ? "diversity" : val.category;
+                var classes = ["emojione", "emojione-32-" + sheet, "_" + unicode, val.category];
 
+                span.id = unicode;
                 span.title = val.name;
-                span.setAttribute("data-unicode", val.unicode);
+                span.setAttribute("data-unicode", unicode);
                 if (typeof val.tone !== 'undefined') classes.push("tone-" + val.tone);
                 span.className = classes.join(" ");
 
@@ -19,7 +22,7 @@ Office.initialize = function () {
                     if (asyncResult.status === Office.AsyncResultStatus.Succeeded) {
                         var html = asyncResult.value === Office.MailboxEnums.BodyType.Html;
                         var textToInsert = (html && !forceText) ?
-                            '<img width="20" height="20" align="middle" style="width: 3ex; height: 3ex; min-width: 20px; min-height: 20px; display: inline-block; margin: 0 .15em .2ex; line-height: normal; vertical-align: middle" class="emojione" alt="' + emoji + '" src="' + 'https://cdnjs.cloudflare.com/ajax/libs/emojione/2.2.6/assets/png/' + unicode + '.png">'
+                            '<img width="20" height="20" align="middle" style="width: 3ex; height: 3ex; min-width: 20px; min-height: 20px; display: inline-block; margin: 0 .15em .2ex; line-height: normal; vertical-align: middle" class="emojione" alt="' + emoji + '" src="' + 'https://cdn.jsdelivr.net/emojione/assets/3.0/png/64/' + unicode + '.png">'
                             : emoji;
                         Office.context.mailbox.item.body.setSelectedDataAsync(
                             textToInsert,
@@ -31,7 +34,7 @@ Office.initialize = function () {
                                             type: "errorMessage",
                                             message: "Failed to insert emoji \"" + emoji + "\": " + asyncResult.error.message
                                         });
-                                    }    
+                                    }
                                 }
                             });
                     } else {
@@ -40,12 +43,12 @@ Office.initialize = function () {
                                 type: "errorMessage",
                                 message: "Failed to insert emoji \"" + emoji + "\": " + asyncResult.error.message
                             });
-                        }    
+                        }
                     }
                 });
             }
         };
 
         Emoji(options);
-    })
-}
+    });
+};
