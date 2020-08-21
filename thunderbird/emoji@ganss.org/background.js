e0e2e7abf61cbb204fdd9cd254ae9307e8f22cec
Michael Ganss <michael.ganss@updatestar.com>
2020-08-21 16:57:51 +0200

    Make chat work

--- thunderbird/emoji@ganss.org/background.js
+++ thunderbird/emoji@ganss.org/background.js
@@ -5,9 +5,9 @@ messenger.WindowListener.registerChromeUrl([
     ["locale",  "emoji", "de-DE",     "chrome/locale/de-DE/"]
 ]);
 messenger.WindowListener.registerOptionsPage("chrome://emoji/content/preferences.xul");
-// messenger.WindowListener.registerWindow(
-//     "chrome://messenger/content/messenger.xul", 
-//     "chrome://emoji/content/messenger.js");
+messenger.WindowListener.registerWindow(
+    "chrome://messenger/content/messenger.xul", 
+    "chrome://emoji/content/messenger.js");
 messenger.WindowListener.registerWindow(
     "chrome://messenger/content/messengercompose/messengercompose.xul", 
     "chrome://emoji/content/messengercompose.js");
--- thunderbird/emoji@ganss.org/chrome/content/messenger.js
+++ thunderbird/emoji@ganss.org/chrome/content/messenger.js
@@ -1,15 +1,38 @@
 // Import any needed modules.
 var { Services } = ChromeUtils.import("resource://gre/modules/Services.jsm");
 
-// Load an additional JavaScript file.
-Services.scriptloader.loadSubScript("chrome://quickfolders/content/quickfolders.js", this, "UTF-8");
-
-// Conditionally load a JavaScript file for the windows plattform
-if (Services.appinfo.OS == "WINNT") {
-  Services.scriptloader.loadSubScript("chrome://quickfolders/content/quickfolders_windows.js", this, "UTF-8");
-}
-
 function onLoad(activatedWhileWindowOpen) {
+    let os = Services.appinfo.OS;
+    let skin = os === "Darwin" ? "mac" : (os === "Linux" ? "linux" : "win"); 
+
+    WL.injectCSS("chrome://emoji/content/skin/style.css");
+    WL.injectCSS(`chrome://emoji/content/skin/${skin}/emoji.css`);
+
+    let xul = `<menupopup id="menu_View_Popup">
+        <menuitem id="menu_EmojiSidebar" insertafter="viewAttachmentsInlineMenuitem" label="&emojiSidebar.label;" accesskey="&emojiSidebar.accesskey;" type="checkbox" oncommand="EmojiOverlay.toggleEmoji();" />
+      </menupopup>
+      <toolbar id="chat-toobar">
+        <toolbarbutton class="toolbarbutton-1" insertafter="gloda-im-search"
+          id="button-emoji" label="&emojiButton.label;"
+          tooltiptext="&emojiButton.tooltip;"
+          oncommand="EmojiOverlay.toggleEmoji();" removable="true" />
+      </toolbar>
+      <hbox flex="1" id="chatPanel">
+        <splitter id="emoji-splitter" insertafter="chat-notification-top" />
+        <vbox id="emoji-box" persist="sidebarVisible width" hidden="true" insertafter="emoji-splitter">
+          <sidebarheader id="emoji-header" align="center">
+            <label id="emoji-title" value="Emoji" />
+            <spacer flex="1"/>
+            <toolbarbutton class="ab-closebutton close-icon" oncommand="EmojiOverlay.toggleEmoji();" />
+          </sidebarheader>
+          <browser id="emoji" flex="1" src="" disablehistory="true" tooltip="browserTooltip" />
+          <tooltip id="browserTooltip" onpopupshowing="return EmojiOverlay.fillTooltip(this)" />
+        </vbox>
+      </hbox>`;
+
+    WL.injectElements(xul, ["chrome://emoji/locale/emoji.dtd"], true);
+
+    Services.scriptloader.loadSubScript("chrome://emoji/content/overlay.js", this, "UTF-8");
 }
 
 function onUnload(deactivatedWhileWindowOpen) {
